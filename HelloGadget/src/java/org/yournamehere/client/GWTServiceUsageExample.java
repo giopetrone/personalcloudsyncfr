65c747862ef53442ccb33c44bd493d645de63595
gio.petrone@gmail.com <gio.petrone@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2009-09-03 11:51:28 +0000

    ggg

---
+++ HelloGadget/src/java/org/yournamehere/client/GWTServiceUsageExample.java
@@ -0,0 +1,75 @@
+/*
+ * GWTServiceUsageExample.java
+ *
+ * Created on April 9, 2009, 2:08 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.yournamehere.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Example class using the GWTService service.
+ *
+ * @author giovanna
+ */
+public class GWTServiceUsageExample extends VerticalPanel {
+    private Label lblServerReply = new Label();
+    private TextBox txtUserInput = new TextBox();
+    private Button btnSend = new Button("Send to server");
+    
+    public GWTServiceUsageExample() {
+        add(new Label("Input your text: "));
+        add(txtUserInput);
+        add(btnSend);
+        add(lblServerReply);
+        
+        // Create an asynchronous callback to handle the result.
+        final AsyncCallback callback = new AsyncCallback() {
+            public void onSuccess(Object result) {
+                lblServerReply.setText((String)result);
+            }
+            
+            public void onFailure(Throwable caught) {
+                lblServerReply.setText("Communication failed");
+            }
+        };
+        
+        // Listen for the button clicks
+        btnSend.addClickListener(new ClickListener(){
+            public void onClick(Widget w) {
+                // Make remote call. Control flow will continue immediately and later
+                // 'callback' will be invoked when the RPC completes.
+                getService().myMethod(txtUserInput.getText(), callback);
+            }
+        });
+    }
+    
+    public static GWTServiceAsync getService(){
+        // Create the client proxy. Note that although you are creating the
+        // service interface proper, you cast the result to the asynchronous
+        // version of
+        // the interface. The cast is always safe because the generated proxy
+        // implements the asynchronous interface automatically.
+        GWTServiceAsync service = (GWTServiceAsync) GWT.create(GWTService.class);
+        // Specify the URL at which our service implementation is running.
+        // Note that the target URL must reside on the same domain and port from
+        // which the host page was served.
+        //
+        ServiceDefTarget endpoint = (ServiceDefTarget) service;
+        String moduleRelativeURL = GWT.getModuleBaseURL() + "gwtservice";
+        endpoint.setServiceEntryPoint(moduleRelativeURL);
+        return service;
+    }
+}
