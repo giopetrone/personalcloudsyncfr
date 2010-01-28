882c2f01183ebcb287a99fda808728ec2b50b695
gio.petrone@gmail.com <gio.petrone@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2010-01-28 11:20:02 +0000

    commentata finestra di debug

---
+++ Parroty/src/java/wave/wavejavascript.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+<?xml version="1.0" encoding="UTF-8" ?>
+<Module>
+  <ModulePrefs title="Hello Wave">
+    <Require feature="wave" />
+  </ModulePrefs>
+  <Content type="html">
+    <![CDATA[
+       Hello, Wave!
+    ]]>
+  </Content>
+</Module>
