1916d9ac4bd3715b2f7176cc2de227afc50c8728
gio.petrone@gmail.com <gio.petrone@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2010-09-06 14:53:57 +0000

    I version of NotifMgr solo x IM

--- MailLib/src/maillib/SendMailCl.java
+++ MailLib/src/maillib/SendMailCl.java
@@ -2,12 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package maillib;
 
 /**
  *
  * @author giovanna
  */
+
+
 import java.security.Security;
 import java.util.Properties;
 
@@ -21,74 +24,62 @@ import javax.mail.internet.MimeMessage;
 
 public class SendMailCl {
 
-    private String SMTP_HOST_NAME = "smtp.gmail.com";
-    private String SMTP_PORT = "465";
-    private String emailMsgTxt = "Test Message Contents";
-    private String emailSubjectTxt = "A test from gmail";
-    private String emailFromAddress = "sgnmrn@gmail.com";
-    private String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-    private String[] recipients = {"gio.petrone@gmail.com", "fabrizio.torretta@gmail.com"};
+private static final String SMTP_HOST_NAME = "smtp.gmail.com";
+private static final String SMTP_PORT = "465";
+private static final String emailMsgTxt = "Test Message Contents";
+private static final String emailSubjectTxt = "A test from gmail";
+private static final String emailFromAddress = "fabrizio.torretta@gmail.com";
+private static final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+private static final String[] sendTo = { "gio.petrone@gmail.com", "fabrizio.torretta@gmail.com"};
+private static final String pwd = "gregorio";
 
-    public SendMailCl(String[] sendTo, String emailSubjectTxt, String emailMsgTxt, String emailFromAddress) {
+public static void main(String args[]) throws Exception {
 
-        this.emailMsgTxt = emailMsgTxt;
-        this.emailSubjectTxt = emailSubjectTxt;
-        this.emailFromAddress = emailFromAddress;
-        this.recipients = sendTo;
-    }
+Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
 
-    public SendMailCl() {
-    }
+new SendMailCl().sendSSLMessage(sendTo, emailSubjectTxt, emailMsgTxt, emailFromAddress,pwd);
+System.out.println("Sucessfully Sent mail to All Users");
+}
 
-    public static void main(String args[]) throws Exception {
 
-        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        String[] sT = {"gio.petrone@gmail.com", "fabrizio.torretta@gmail.com"};
-        SendMailCl sm = new SendMailCl(sT, "A test from gmail22", "Test Message Contents", "sgnmrn@gmail.com");
-        sm.sendSSLMessage();
-        System.out.println("Sucessfully Sent mail to All Users");
-    }
 
-    //    public void sendSSLMessage(String recipients[], String subject, String message, String from)
-    public void sendSSLMessage()
-            throws MessagingException {
-        boolean debug = true;
+public void sendSSLMessage(String recipients[], String subject,
+String message,final String from, final String pwd) throws MessagingException {
+boolean debug = true;
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", SMTP_HOST_NAME);
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.smtp.port", SMTP_PORT);
-        props.put("mail.smtp.socketFactory.port", SMTP_PORT);
-        props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
-        props.put("mail.smtp.socketFactory.fallback", "false");
+Properties props = new Properties();
+props.put("mail.smtp.host", SMTP_HOST_NAME);
+props.put("mail.smtp.auth", "true");
+props.put("mail.debug", "true");
+props.put("mail.smtp.port", SMTP_PORT);
+props.put("mail.smtp.socketFactory.port", SMTP_PORT);
+props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
+props.put("mail.smtp.socketFactory.fallback", "false");
 
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
+Session session = Session.getDefaultInstance(props,
+new javax.mail.Authenticator() {
 
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("sgnmrn@gmail.com", "micio11");
+protected PasswordAuthentication getPasswordAuthentication() {
+return new PasswordAuthentication(from, pwd);
 
-                    }
-                });
+}
+});
 
-        session.setDebug(debug);
+session.setDebug(debug);
 
-        Message msg = new MimeMessage(session);
-        InternetAddress addressFrom = new InternetAddress(emailFromAddress);
-        msg.setFrom(addressFrom);
+Message msg = new MimeMessage(session);
+InternetAddress addressFrom = new InternetAddress(from);
+msg.setFrom(addressFrom);
 
-        InternetAddress[] addressTo = new InternetAddress[recipients.length];
-        for (int i = 0; i < recipients.length; i++) {
-            addressTo[i] = new InternetAddress(recipients[i]);
-        }
-        msg.setRecipients(Message.RecipientType.TO, addressTo);
+InternetAddress[] addressTo = new InternetAddress[recipients.length];
+for (int i = 0; i < recipients.length; i++) {
+addressTo[i] = new InternetAddress(recipients[i]);
+}
+msg.setRecipients(Message.RecipientType.TO, addressTo);
 
 // Setting the Subject and Content Type
-        msg.setSubject(emailMsgTxt);
-        msg.setContent(emailSubjectTxt, "text/plain");
-        Transport.send(msg);
-    }
-} 
-
- 
+msg.setSubject(subject);
+msg.setContent(message, "text/plain");
+Transport.send(msg);
+}
+}
