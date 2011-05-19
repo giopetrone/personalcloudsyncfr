e4125d2a4a8430688a5c62df182c3e08ccf779d1
gio.petrone@gmail.com <gio.petrone@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2011-05-19 17:22:25 +0000

    Web project per mail events Hub

---
+++ HubEventsMgr/src/java/web/MailHubEvents.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import com.thoughtworks.xstream.XStream;
+import java.util.ArrayList;
+import maillib.HubMailMsg;
+import maillib.NewGmail;
+
+/**
+ *
+ * @author giovanna
+ */
+public class MailHubEvents {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        NewGmail gmail = new NewGmail();
+//        ArrayList<HubMailMsg> msg = gmail.getMailMessages();
+//        if (msg.size() > 0) {
+//            HubMailMsg hm = msg.get(0);
+//            // hm.getTextContent(msg[0]); 
+//
+//            String s = toXML(hm);
+//            System.out.println("messaggio in XML \n " + s);
+//
+//        }
+
+    }
+
+    // public static String toXML(HubMailMsg hm) {
+    public String toXML(HubMailMsg hm) {
+        XStream xstream = new XStream();
+        String s = xstream.toXML(hm);
+        return s;
+    }
+
+  //   public static HubMailMsg fromXml(String s) {
+    public   HubMailMsg fromXml(String s) {
+        XStream xstream = new XStream();
+        try {
+            Object ob = xstream.fromXML(s);
+            if (ob.getClass() == maillib.HubMailMsg.class) {
+
+                return (HubMailMsg) ob;
+            }
+        } catch (com.thoughtworks.xstream.io.StreamException ex) {
+            System.err.println("xstream, getEvent, error in HubMailMsg content = " + s);
+        }
+        return null;
+    }
+}
---
+++ HubEventsMgr/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : May 19, 2011, 5:31:04 PM
+    Author     : giovanna
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
