038115a5223445f670495e60cc72f3dee09ac730
sgnmrn@gmail.com <sgnmrn@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2011-06-02 12:44:15 +0000

    prova

--- MailLib/src/maillib/NewGmail.java
+++ MailLib/src/maillib/NewGmail.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import javax.mail.Authenticator;
 import javax.mail.Folder;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
@@ -28,31 +29,38 @@ public class NewGmail {
     private static final int SMTP_HOST_PORT = 465;
     private static final String SMTP_AUTH_USER = "sgnmrn@gmail.com";
     private static final String SMTP_AUTH_PWD = "micio11";
+    private static final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
     private static boolean debug = true;
+    private static Session mailSession;
 
     public static void main(String[] args) throws Exception {
         NewGmail gMail = new NewGmail();
-        //  gMail.testSendMail();
-        gMail.getMailMessages();
+        gMail.sendMail("sgnmrn@gmail.com", "esempio mail");
+        gMail = new NewGmail();
+        gMail.sendMail("sgnmrn@gmail.com", "esempio mail");
+        // gMail.getMailMessages();
     }
 
-    public void testSendMail() throws Exception {
+    public void sendMail(String user, String content) throws Exception {
         Properties props = new Properties();
 
         props.put("mail.transport.protocol", "smtps");
         props.put("mail.smtps.host", SMTP_HOST_NAME);
         props.put("mail.smtps.auth", "true");
         // props.put("mail.smtps.quitwait", "false");
-        Session mailSession = Session.getDefaultInstance(props);
-        mailSession.setDebug(true);
+        if (mailSession == null) {
+            //     mailSession = Session.getDefaultInstance(props);
+            mailSession = Session.getInstance(props);
+            mailSession.setDebug(true);
+        }
         Transport transport = mailSession.getTransport();
 
         MimeMessage message = new MimeMessage(mailSession);
         message.setSubject("Testing SMTP-SSL");
-        message.setContent("This is a test", "text/plain");
+        message.setContent(content, "text/plain");
 
         message.addRecipient(Message.RecipientType.TO,
-                new InternetAddress("sgnmrn@gmail.com"));
+                new InternetAddress(user));
         //  new InternetAddress("gio.petrone@gmail.com"));
 
         transport.connect(SMTP_HOST_NAME, SMTP_HOST_PORT, SMTP_AUTH_USER, SMTP_AUTH_PWD);
@@ -60,6 +68,46 @@ public class NewGmail {
         transport.sendMessage(message,
                 message.getRecipients(Message.RecipientType.TO));
         transport.close();
+    }
+
+    public void sendSSLMessage(String recipients[], String subject,
+            String message, final String from, final String pwd) throws Exception {
+        boolean debug = true;
+   sendMail("sgnmrn@gmail.com", "esempio mail da TaskMgr");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", SMTP_HOST_NAME);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.port", SMTP_HOST_PORT);
+        props.put("mail.smtp.socketFactory.port", SMTP_HOST_PORT);
+        props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, pwd);
+
+                    }
+                });
+
+        session.setDebug(debug);
+
+        Message msg = new MimeMessage(session);
+        InternetAddress addressFrom = new InternetAddress(from);
+        msg.setFrom(addressFrom);
+
+        InternetAddress[] addressTo = new InternetAddress[recipients.length];
+        for (int i = 0; i < recipients.length; i++) {
+            addressTo[i] = new InternetAddress(recipients[i]);
+        }
+        msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+// Setting the Subject and Content Type
+        msg.setSubject(subject);
+        msg.setContent(message, "text/plain");
+        Transport.send(msg);
     }
 
     public ArrayList<HubMailMsg> getMailMessages() {
@@ -82,9 +130,9 @@ public class NewGmail {
             // Display message.
 
             for (int i = 0; i < messages.length; i++) {
-                HubMailMsg hubMsg = new HubMailMsg(messages[i]);             
+                HubMailMsg hubMsg = new HubMailMsg(messages[i]);
                 messaRet.add(hubMsg);
-                System.out.println("Body : " +hubMsg.getContent());
+                System.out.println("Body : " + hubMsg.getContent());
                 if (debug) {
                     System.out.println("------------ Message " + (i + 1) + " ------------");
 
