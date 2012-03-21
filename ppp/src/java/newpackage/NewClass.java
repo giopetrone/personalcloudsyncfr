c43c98b4088a0c294a83a934dc30b82d7e70d343
sgnmrn@gmail.com <sgnmrn@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2012-03-21 10:08:30 +0000

    iniziale

---
+++ ppp/src/java/newpackage/NewClass.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import com.gdevelop.gwt.syncrpc.SyncProxy;
+import java.util.ArrayList;
+import org.yournamehere.client.GWTService;
+
+/**
+ *
+ * @author marino
+ */
+public class NewClass {
+
+    private static GWTService rpcService = null;
+
+    public static void main(String[] args) {
+
+        rpcService = (GWTService) SyncProxy.newProxyInstance(GWTService.class,
+                "http://localhost:8080/ppp/org.yournamehere.Main/", "gwtservice");
+
+        String result = rpcService.myMethod("SyncProxy");
+
+        System.out.println("result=" + result);
+
+        ArrayList<String> ret = new ArrayList();
+        ret.add("sadDFFF");
+        ret.add("BBBBBBB");
+        ret.add("wqeqdSSSSS");
+
+        ArrayList<String> re = rpcService.myMethod1(ret);
+        
+        for (String de : re) {
+            System.out.println("bassa=" + de);
+        }
+    }
+}
