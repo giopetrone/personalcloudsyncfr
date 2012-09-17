8426918f325f468183875668bffa359dfb83f4fe
gio.petrone@gmail.com <gio.petrone@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2012-09-17 10:18:32 +0000

    works !

--- Spike2_GWTGAEApp/src/dida/spikes3/server/ProxyServlet.java
+++ Spike2_GWTGAEApp/src/dida/spikes3/server/ProxyServlet.java
@@ -15,76 +15,71 @@ import org.json.JSONTokener;
 
 public class ProxyServlet extends HttpServlet {
 
-	 private static final long serialVersionUID = -6455653509373554816L;
-	 
-	 @Override
-     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-                     throws ServletException, IOException {
-		    InputStream is = req.getInputStream();
-		    PrintWriter pw = resp.getWriter();
-		    pw.print("ciao da anna");
-		    pw.flush();
-            pw.close();
-	 }
-	 
-	 @Override
-     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-                     throws ServletException, IOException {
+	private static final long serialVersionUID = -6455653509373554816L;
 
-             InputStream is = req.getInputStream();             
-             PrintWriter pw = resp.getWriter();
-            try {
-                     JSONTokener jt = new JSONTokener(is);
-                     JSONObject jo = new JSONObject(jt);
-                     JSONObject rj = new JSONObject();    
-                   //temp anangio
-                     rj.put("status", "OK");                     
-                     String request = jo.getString("request");                
-                      
-                     if (request.equals("firstTest"))
-                    	                 
-                         rj.put("status", "OK");
-                     else {                             
-                             rj.put("status", "ERROR");                              
-                           
-                     }
-                     rj.put("status", "OK");
-                     
-                     String json = "{"
-                	         + "  \"query\": \"Pizza\", "
-                	         + "  \"locations\": [ 94043, 90210 ] "
-                	         + "}";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		InputStream is = req.getInputStream();
+		PrintWriter pw = resp.getWriter();
+		pw.print("ciao da anna");
+		pw.flush();
+		pw.close();
+	}
 
-                	 JSONObject object = (JSONObject) new JSONTokener(json).nextValue();
-                	 String query = object.getString("query");
-                	 JSONArray locations = object.getJSONArray("locations");
-                     
-                     pw.print(object);
-                     pw.flush();
-                    
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		InputStream is = req.getInputStream();
+		PrintWriter pw = resp.getWriter();
+		try {
+			
+			JSONTokener jt = new JSONTokener(is);
+			JSONObject jo = new JSONObject(jt);
+			JSONObject rj = new JSONObject();
+			// temp anangio
+		 
+			String request = jo.getString("request");
+
+			if (request.equals("firstTest"))
+
+				rj.put("status", "OK");
+			else {
+				rj.put("status", "ERROR");
+
+			}
+			  
+			//JSONObject object = (JSONObject) new JSONTokener(json).nextValue();
+			//String query = object.getString("food");
+		//	JSONArray locations = object.getJSONArray("locations");
  
-             } catch (Exception e) {
-                     System.err.println("Error while processing post request.");
-                     System.err.println(e);
-             }
-	  finally  { pw.close();}
+			pw.print(rj);
+			pw.flush();
 
-	 }
-	 
-     private void createResult(JSONObject jo, PrintWriter pw) {
-         try {
-                 //String email = jo.getString("userEmail");
-                 
-                 JSONObject rj = new JSONObject();
-                 
-                 rj.put("status", "OK");
+		} catch (Exception e) {
+			System.err.println("Error while processing post request.");
+			System.err.println(e);
+		} finally {
+			pw.close();
+		}
 
-         } catch (Exception e) {
-                 System.err.println("Error while querying user");
-                 System.err.println(e);
-         } finally {
-                 pw.close();
-         }
- }
+	}
+
+	private void createResult(JSONObject jo, PrintWriter pw) {
+		try {
+			// String email = jo.getString("userEmail");
+
+			JSONObject rj = new JSONObject();
+
+			rj.put("status", "OK");
+
+		} catch (Exception e) {
+			System.err.println("Error while querying user");
+			System.err.println(e);
+		} finally {
+			pw.close();
+		}
+	}
 
 }
