26bf290515b5a66ff7674dad508fadfee4c0e9dd
sgnmrn@gmail.com <sgnmrn@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2012-09-26 10:42:54 +0000

    ciao

--- ProvaDeploy2/src/mui/client/services/TokenServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package mui.client.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface TokenServiceAsync {
-	void getToken(String requestUri, AsyncCallback<String> callback);
-	void getDocsInfo(String token, AsyncCallback<String> callback);
-}
