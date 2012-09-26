0407e87bee98f05cabeab5d071760ba4960a7235
sgnmrn@gmail.com <sgnmrn@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2012-09-26 10:42:54 +0000

    ciao

--- ProvaDeploy2/src/mui/client/services/LoginServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package mui.client.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-	public void isLogged(String requestUri, AsyncCallback<String> callback);
-}
