ff6add705dcbaab9c9a4b586aeae6cda58f096bd
alfonso.disalvo@gmail.com <alfonso.disalvo@gmail.com@d1ff346a-828c-11de-9a75-9d4ddcaa5d2c>
2013-04-04 12:41:13 +0000

    Per l’ integrazione di GCM sono stati fatti inizialmente tre spikes  per testare il funzionamento di questo servizio. Nel repository condiviso sono presenti i seguenti progetti che si concentrano sul funzionamento di GCM: GCMClientAndroid che rappresenta un semplice client Android correttamente configurato che comunica con il server di GCM; GCMDemoClient e GCMDemo4 che sono rispettivamente un client Android e il Server usando Google App Engine per Java e che mostrano in maniera completa come avviene la comunicazione tra Client e Server utilizzando GCM (questi ultimi due progetti sono quelli che poi sono stati integrati in TablePlusTablet apportando le opportune modifiche). Si ricorda sempre di utilizzare emulatori con le Google APIs.

---
+++ GCMDemoClient/src/com/google/android/gcm/demo/app/DemoActivity.java
@@ -0,0 +1,148 @@
+package com.google.android.gcm.demo.app;
+
+import com.google.android.gcm.GCMRegistrar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import static com.google.android.gcm.demo.app.CommonUtilities.DISPLAY_MESSAGE_ACTION;
+import static com.google.android.gcm.demo.app.CommonUtilities.EXTRA_MESSAGE;
+import static com.google.android.gcm.demo.app.CommonUtilities.SENDER_ID;
+import static com.google.android.gcm.demo.app.CommonUtilities.SERVER_URL;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DemoActivity extends Activity {
+
+	   TextView mDisplay;
+	   AsyncTask<Void, Void, Void> mRegisterTask;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        checkNotNull(SERVER_URL, "SERVER_URL");
+        checkNotNull(SENDER_ID, "SENDER_ID");
+        
+        GCMRegistrar.checkDevice(this);
+        GCMRegistrar.checkManifest(this);
+        setContentView(R.layout.main);
+        
+        mDisplay = (TextView) findViewById(R.id.display);
+        
+        registerReceiver(mHandleMessageReceiver,
+                new IntentFilter(DISPLAY_MESSAGE_ACTION));
+        
+        final String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+            // Automatically registers application on startup.
+            GCMRegistrar.register(this, SENDER_ID);
+        } else {
+        	
+            // Device is already registered on GCM, check server.
+            if (GCMRegistrar.isRegisteredOnServer(this)) {
+                // Skips registration.
+                mDisplay.append(getString(R.string.already_registered) + "\n");
+            } else {
+                // Try to register again, but not in the UI thread.
+                // It's also necessary to cancel the thread onDestroy(),
+                // hence the use of AsyncTask instead of a raw thread.
+                final Context context = this;
+                mRegisterTask = new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        boolean registered =
+                                ServerUtilities.register(context, regId);
+                        // At this point all attempts to register with the app
+                        // server failed, so we need to unregister the device
+                        // from GCM - the app will try to register again when
+                        // it is restarted. Note that GCM will send an
+                        // unregistered callback upon completion, but
+                        // GCMIntentService.onUnregistered() will ignore it.
+                        if (!registered) {
+                            GCMRegistrar.unregister(context);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        mRegisterTask = null;
+                    }
+
+                };
+                mRegisterTask.execute(null, null, null);
+            }
+        }
+        System.out.println("ciao");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            /*
+             * Typically, an application registers automatically, so options
+             * below are disabled. Uncomment them if you want to manually
+             * register or unregister the device (you will also need to
+             * uncomment the equivalent options on options_menu.xml).
+             */
+            /*
+            case R.id.options_register:
+                GCMRegistrar.register(this, SENDER_ID);
+                return true;
+            case R.id.options_unregister:
+                GCMRegistrar.unregister(this);
+                return true;
+             */
+            case R.id.options_clear:
+                mDisplay.setText(null);
+                return true;
+            case R.id.options_exit:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mRegisterTask != null) {
+            mRegisterTask.cancel(true);
+        }
+        unregisterReceiver(mHandleMessageReceiver);
+        GCMRegistrar.onDestroy(this);
+        super.onDestroy();
+    }
+
+    private void checkNotNull(Object reference, String name) {
+        if (reference == null) {
+            throw new NullPointerException(
+                    getString(R.string.error_config, name));
+        }
+    }
+
+    private final BroadcastReceiver mHandleMessageReceiver =
+            new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+            mDisplay.append(newMessage + "\n");
+        }
+    };
+}
